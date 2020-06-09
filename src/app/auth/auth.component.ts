b98d2ad87a38a625c7170b62750e6ae0f5c1a7cc
Htun Htun <htunhtun1997913@gmail.com>
2020-06-09 23:27:03 +0630

    create login & sign up component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,53 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { AuthResponseData } from "./auth.service";
+
+@Component({
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+})
+export class AuthComponent {
+  isLoginMode = true;
+  isLoading = false;
+  error: String = null;
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      this.authService.signup(email, password);
+    }
+
+    authObs.subscribe(
+      (response) => {
+        console.log(response);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    form.reset();
+  }
+}
